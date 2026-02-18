a8f42820d946adfe17ca666852be5a3786586e0f
Gunavardhan <yajjalainfi@gmail.com>
2026-02-18 21:07:08 +0530

    Fix bugs

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
 from flask import Flask, request
-from numlang import run_code  # your interpreter
+from flask_cors import CORS
+from numlang import run_code
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/run", methods=["POST"])
 def run():
     data = request.get_json()
     code = data.get("code", "")
+
     try:
-        output = run_code(code)  # your numlang.py must have run_code function
-        return output
+        return run_code(code)
     except Exception as e:
         return str(e)
 
 if __name__ == "__main__":
     app.run(port=5000)
-
